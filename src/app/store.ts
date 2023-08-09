b5f120a4e60ae62c0ec10a42d6f1895580b62c1e
Michael <diachenko_md@hotmail.com>
2023-08-09 10:57:16 +0300

    Added storage persistence with redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { api } from "./api"
-import { authSlice } from "@features/auth/authSlice";
+import { authSlice } from "@features/auth/authSlice"
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+const persistedAuthReducer = persistReducer(
+  {
+    key: "auth",
+    storage,
+  },
+  authSlice.reducer,
+)
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    [authSlice.name]: authSlice.reducer,
+    [authSlice.name]: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 })
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "@app/store"
+import { persistor, store } from "@app/store"
 import "./index.css"
 import App from "./App"
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { PersistGate } from "redux-persist/integration/react"
 
 // [NOTE: good description of variants styling: https://github.com/chakra-ui/chakra-ui/issues/7556]
 
@@ -67,9 +68,11 @@ const theme = extendTheme({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
